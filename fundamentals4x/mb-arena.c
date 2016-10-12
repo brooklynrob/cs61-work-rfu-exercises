f625ef86f3cea35170d4ecbb4a79f39f2d594696
Robert Frederick Underwood <runderwood5@gmail.com>
2016-10-12 15:01:40 +0000

    Finished working through Ex 4x mb-arena.c through sol video

--- fundamentals4x/mb-arena.c
+++ fundamentals4x/mb-arena.c
@@ -21,16 +21,24 @@ See "membench.h" for function semantics.
 
 */
 
+typedef union chunk_or_free {
+     chunk c;
+     union chunk_or_free* next;
+} chunk_or_free;
+
+
+//chunk is defined in membench.h as a struct with element, an array of char, based on a constant, chunksize, which is 8
 
 #define GROUPSIZE 4096
 typedef struct membench_group {
     int pos;
-    chunk chunks[GROUPSIZE];
+    chunk_or_free chunks[GROUPSIZE];
     struct membench_group* next;
 } membench_group;
 
 struct membench_arena {
     membench_group* group;
+    chunk_or_free* free_list;
 };
 
 
@@ -43,10 +51,22 @@ static membench_group* membench_group_new(void) {
 membench_arena* membench_arena_create(void) {
     membench_arena* arena = (membench_arena*) malloc(sizeof(membench_arena));
     arena->group = membench_group_new();
+    arena->free_list = NULL;
     return arena;
 }
 
 chunk* membench_alloc(membench_arena* arena) {
+    if (arena->free_list) {
+        // pop a free chunk, return it
+        chunk* result = &arena->free_list->c; //this is a pointer to a chunk for free
+        //chunk* result = &arena->free_list.c; //would produce this error --> error: request for member ‘c’ in something not a structure or union
+        //chunk* result = (chunk*) arena->free_list;      // alternative
+        arena->free_list = arena->free_list->next;
+        return result;
+
+    }
+
+
     membench_group* g = arena->group;
     if (g->pos == GROUPSIZE) {
         // allocate new group, add it to singly-linked group list
@@ -54,12 +74,19 @@ chunk* membench_alloc(membench_arena* arena) {
         g->next = arena->group;
         arena->group = g;
     }
-    chunk* result = &g->chunks[g->pos];
+    //chunk* result = (chunk*) &g->chunks[g->pos]; //need to cast this as (*chunk) since we've created the chunk_or_free
+    chunk* result = (chunk*) &g->chunks[g->pos].c; //this is a chunk or free
     ++g->pos;
     return result;
 }
 
 void membench_free(membench_arena* arena, chunk* x) {
+    chunk_or_free* cf = (chunk_or_free*) x;
+    cf->next = arena->free_list;
+    arena->free_list = cf;
+    
+    //put this on a list of things for allocation later
+    
     (void) arena, (void) x;
 }
 
